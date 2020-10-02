15dfff622d021dd75f976379498181bcc9d531db
Aseem <aseemanand2@gmail.com>
2020-10-02 15:22:42 +0530

    [Aseem] Added new contact in addressbook and viewed

---
+++ AddressBookMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBookMain
+    {
+        public static List<Contact> contactList = new List<Contact>();
+        public void addNewContact(string firstName, string lastName, string address, string city, string state, double zip, double phoneNumber, string email)
+        {
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+            contactList.Add(contact);
+            Console.WriteLine("Contact " + firstName + " " + lastName + " added successfully");
+
+        }
+        public void viewContact(string firstName, string lastName)
+        {
+            bool flag = true;
+            Console.WriteLine("\nDetails of " + firstName + " " + lastName + ":");
+            foreach (var v in contactList)
+            {
+                if (firstName == v.firstName && lastName == v.lastName)
+                {
+                    Console.WriteLine("FullName: " + v.firstName + " " + v.lastName + "\nAddress: " + v.address + "\nCity: " + v.city + "\nState: " + v.state + "\nZip: " + v.zip + "\nPhoneNumber: " + v.phoneNumber + "\nEmail: " + v.email);
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true)
+                Console.WriteLine("Contact not found");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Welcome to Address Book Program\n");
+            AddressBookMain addressBook = new AddressBookMain();
+            addressBook.addNewContact("Ram", "Kumar", "Chandigarh", "Mohali", "Punjab", 194485, 7684654, "abshs@abc.com");
+            addressBook.viewContact("Ram", "Kumar");
         }
     }
 }
