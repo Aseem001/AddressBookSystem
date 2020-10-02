c094a1f6731676fc9ba378e6985c5f234fe36d36
Aseem <aseemanand2@gmail.com>
2020-10-02 16:59:54 +0530

    [Aseem].[Refactor] Added multiple address book to the system

---
+++ AddressBookList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBookList
+    {        
+        public Dictionary<string, AddressBookMain> addressBookListDictionary = new Dictionary<string, AddressBookMain>();
+        public void addNewAddressBook(string addressBookName, AddressBookMain addressBook)
+        {            
+            addressBookListDictionary.Add(addressBookName, addressBook);
+            Console.WriteLine("\nAddress Book "+addressBookName+" added successfully");
+            foreach(var kvp in addressBookListDictionary)
+            {
+                Console.WriteLine("Updated Address Book List:\nKey: {0}, Value: {1}",kvp.Key,kvp.Value);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,17 +7,20 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO ADDRESS BOOK PROGRAM\n");
-            AddressBookMain addressBook = new AddressBookMain();
-            addressBook.addNewContact("Aseem", "Anand", "NITKKR", "Kurukshetra", "Haryana", 136119, 4272755, "a@abc.com");
-            addressBook.viewContact("Aseem", "Anand");
-            addressBook.addNewContact("Ram", "Kumar", "Chandigarh", "Mohali", "Punjab", 194485, 7684654, "abshs@abc.com");
-            addressBook.viewContact("Ram", "Kumar");
-            addressBook.addNewContact("Mohan", "Kumar", "Chandigarh", "Mohali", "Punjab", 194485, 7684654, "abshs@abc.com");
-            addressBook.viewContact("Mohan", "Kumar");
-            //addressBook.editContact("Ram", "Kumar");
-            //addressBook.viewContact("Ram", "Kumar");
-            //addressBook.deleteContact("Ram", "Kumar");
-            //addressBook.viewContact("Ram", "Kumar");
+            AddressBookMain addressBook1 = new AddressBookMain();
+            AddressBookMain addressBook2 = new AddressBookMain();
+            AddressBookList addressBookList = new AddressBookList();
+            addressBook1.addNewContact("Aseem", "Anand", "NITKKR", "Kurukshetra", "Haryana", 136119, 4272755, "a@abc.com");
+            addressBook1.viewContact("Aseem", "Anand");
+            addressBook2.addNewContact("Ram", "Kumar", "Chandigarh", "Mohali", "Punjab", 194485, 7684654, "abshs@abc.com");
+            addressBook2.viewContact("Ram", "Kumar");
+            addressBook2.editContact("Ram", "Kumar");
+            addressBook2.viewContact("Ram", "Kumar");
+            addressBook2.deleteContact("Ram", "Kumar");
+            addressBook2.viewContact("Ram", "Kumar");
+            addressBookList.addNewAddressBook("Aseem Anand", addressBook1);
+            addressBookList.addNewAddressBook("Ram Kumar", addressBook2);            
+            
 
             Console.ReadKey();
         }
