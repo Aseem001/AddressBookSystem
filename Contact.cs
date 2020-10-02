743ac63becb70eda9173bbe2d382f2428ef78f2f
Aseem <aseemanand2@gmail.com>
2020-10-02 15:13:38 +0530

    [Aseem] Added UC1-Ability to add contacts

--- Contact.cs
+++ Contact.cs
@@ -6,5 +6,25 @@ namespace AddressBookSystem
 {
     class Contact
     {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public double zip;
+        public double phoneNumber;
+        public string email;
+
+        public Contact(string firstName, string lastName, string address, string city, string state, double zip, double phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
     }
 }
